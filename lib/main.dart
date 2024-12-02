1da084cddcc76ac185e38121105c76647e46162b
manasseh <manassehrandriamitsiry@gmail.com>
2024-12-03 01:41:11 +0300

    notification update

--- lib/main.dart
+++ lib/main.dart
@@ -4,25 +4,57 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:awesome_notifications/awesome_notifications.dart';
 import 'controller/theme_controller.dart';
 import 'controller/font_controller.dart';
 import 'controller/color_controller.dart';
 import 'screen/accueil/home_screen.dart';
 import 'screen/accueil/accueil_screen.dart';
+import 'services/version_check_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  // Initialize notification action handler
+  await AwesomeNotifications().initialize(
+    null,
+    [
+      NotificationChannel(
+        channelKey: 'basic_channel',
+        channelName: 'Basic notifications',
+        channelDescription: 'Notification channel for basic tests',
+        defaultColor: const Color(0xFF9D50DD),
+        ledColor: Colors.white,
+      ),
+    ],
+    debug: true,
+  );
   final prefs = await SharedPreferences.getInstance();
   runApp(MyApp(prefs: prefs));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   final SharedPreferences prefs;
+
+  const MyApp({super.key, required this.prefs});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   final FontController fontController = Get.put(FontController());
   final ColorController colorController = Get.put(ColorController());
 
-  MyApp({super.key, required this.prefs});
+  @override
+  void initState() {
+    super.initState();
+    // Initialize notifications and check for updates after app is initialized
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      await VersionCheckService.initializeNotifications();
+      await VersionCheckService.checkForUpdate();
+    });
+  }
 
   ThemeData _getThemeWithFont(ThemeData baseTheme, String fontFamily) {
     final TextTheme textTheme = GoogleFonts.getTextTheme(fontFamily);
@@ -37,9 +69,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final ThemeController themeController = Get.put(ThemeController());
-    themeController.isDarkMode.value = prefs.getBool('isDarkMode') ?? false;
+    themeController.isDarkMode.value = widget.prefs.getBool('isDarkMode') ?? false;
     
-    final bool isFirstTime = prefs.getBool('isFirstTime') ?? true;
+    final bool isFirstTime = widget.prefs.getBool('isFirstTime') ?? true;
     
     return Obx(() {
       final currentFont = fontController.currentFont.value;
